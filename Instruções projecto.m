883d3f1809af844b1f8afcc55000800fddc99693
Arid7 <118507952+Arid7@users.noreply.github.com>
2026-05-13 01:39:58 +0100

    Create Instruções projecto.m

---
+++ Instruções projecto.m
@@ -0,0 +1,18 @@
+Por outro lado, e tal como combinado ontem, abordei a Secretaria da Faculdade e se concordou em fazermos um trabalho prático com defesa na época da frequência, que irá contar como a 1.ª prova. Assim:
+
+1.º) Se organizem em grupos de 3 a 4 pessoas e informem às vossas respectivas delegadas que deverão  organizar a informação numa planilha Excel contendo a identificação dos grupos. 
+
+2.º) Procurem e me apresentem ASAP um formulário físico para cadastro em qualquer instituição (por exemplo deixo abaixo o de uma instituição onde faço mergulho, publicidade à parte 😂). Eu vou validar o form antes de vocês avançarem... 
+
+3.º) Voces irão criar a interface gráfica em JavaFX e todo código necessário para o cadastro ser feito por essa vossa mini-aplicação que deverá registar os dados em ficheiros binários de acesso aleatório. 
+
+Como requisitos do trabalho, deverão usar :
+- Java8 como plataforma de desenvolvimento
+- JavaFX como livraria GUI
+- Apache NetBeans 25 como IDE 
+
+Temos cerca de 3 semanas para a execução até à apresentação e defesa.
+
+É possível configurar o JavaFX como livraria externa...é só seguir o tutorial do IDE que usas, todavia e como alertado no contrato pedagógico inicial, cada um é livre de usar as ferramentas que quiser até estarmos num cenário em que se indique claramente quais os requisitos a serem seguidos.
+
+Aconselhava a usar o Java8 que já tem o JavaFX integrado... É menos uma dor de cabeça
